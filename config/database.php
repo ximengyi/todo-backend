fed78f2b173fd64d706662e35e002530e6b96623
mengyi <1203669692@qq.com>
2024-12-22 17:34:08 +0800

    init plugin

--- config/database.php
+++ config/database.php
@@ -12,4 +12,19 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-return [];
+return  [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => getenv('DB_HOST'),
+            'port'        => getenv('DB_PORT'),
+            'database'    => getenv('DB_NAME'),
+            'username'    => getenv('DB_USER'),
+            'password'    => getenv('DB_PASSWORD'),
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_general_ci',
+            'strict'      => true,
+        ],
+    ],
+];
